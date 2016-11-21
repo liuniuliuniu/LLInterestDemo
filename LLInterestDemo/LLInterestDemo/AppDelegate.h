073caa9f8f94c9574030331974bc299fa952ce8d
liushaohua <18703867769@163.com>
2016-11-21 18:38:38 +0800

    立项

---
+++ LLInterestDemo/LLInterestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLInterestDemo
+//
+//  Created by liushaohua on 16/11/21.
+//  Copyright © 2016年 liushaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLInterestDemo/LLInterestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLInterestDemo
+//
+//  Created by liushaohua on 16/11/21.
+//  Copyright © 2016年 liushaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
